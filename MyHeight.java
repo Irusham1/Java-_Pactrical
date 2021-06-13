96d4534ea755f1b8226a69cde11e44dcb9803cea
Iruka <85832603+Irusham1@users.noreply.github.com>
2021-06-13 19:55:42 +0530

    Add files via upload

---
+++ MyHeight.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;//Import the Scanner class
+class MyHeight{
+	public static void main(String []args){
+		Scanner x=new Scanner(System.in);//Create a Scanner object
+		System.out.println("Enter your height");
+		int p_height = x.nextInt();//Read user input
+	System.out.println(p_height);//Output user input}
+	}
+	}
+	
+		

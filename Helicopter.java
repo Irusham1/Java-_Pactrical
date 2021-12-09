37d4b505e4ebf0ff7871cdae82527916f019fd97
Iruka <85832603+Irusham1@users.noreply.github.com>
2021-12-09 16:15:30 +0530

    Add files via upload

---
+++ Helicopter.java
@@ -0,0 +1,53 @@
+public class Helicopter{
+	public static void main(String[] args) throws InterruptedException {
+
+	while (true) {
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		System.out.println("         :IRU:Irush:Irush");
+		System.out.println("           A           ");
+		System.out.println("       /--------       ");
+		System.out.println(" IRU====      ()\\     ");
+		System.out.println("  L    \\         \\        ");
+		System.out.println("        \\_________]                ");
+		System.out.println("           I     I      ");
+		System.out.println("        -----------/              ");
+		System.out.println("                       ");
+		System.out.println("      Irush Helicopter!              ");
+
+		Thread.sleep(1);
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		System.out.println("Irush:Irush:IRU:         ");
+		System.out.println("           A           ");
+		System.out.println("  L    /--------       ");
+		System.out.println(" IRU====      ()\\     ");
+		System.out.println("       \\         \\        ");
+		System.out.println("        \\_________]                ");
+		System.out.println("           I     I      ");
+		System.out.println("        -----------/              ");
+		System.out.println("                       ");
+		System.out.println("      Irush Helicopter!              ");
+
+		Thread.sleep(1);
+	}
+
+}
+
+}

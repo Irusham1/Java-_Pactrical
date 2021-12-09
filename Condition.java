46efcaa24f5531b19c0dcc38f03d175bd9d65ebf
Iruka <85832603+Irusham1@users.noreply.github.com>
2021-12-09 16:14:11 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;//Import the Scanner class
+class Condition{
+	public static void main(String[] args){
+		boolean isStudy=true;
+		boolean partExam=true;
+		if(isStudy==true && partExam==true){
+			System.out.println("Success");
+		
+		}else if{
+			System.out.println("fail");
+		}
+	}
+}
+
+			
